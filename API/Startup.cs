ca1e94c732d2ea48be03601e3cee8c8ae37c7cff
Vinh Tran <vinhdt90@outlook.com>
2020-11-13 08:55:06 +1100

    moved swagger app and services to own extension class, and removed unused usings

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,18 +1,13 @@
-using API.Errors;
+using API.Extensions;
 using API.Helpers;
 using API.Middleware;
 using AutoMapper;
-using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
-using System.Linq;
 
 namespace API
 {
@@ -35,11 +30,7 @@ namespace API
 
             // add services from extension class
             services.AddApplicationServices();
-
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "SkiNet API", Version = "v1" });
-            });
+            services.AddSwaggerDocumentation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -58,8 +49,7 @@ namespace API
 
             app.UseAuthorization();
 
-            app.UseSwagger();
-            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "SkiNet API v1"); });
+            app.UseSwaggerDocumentation();
 
             app.UseEndpoints(endpoints =>
             {
